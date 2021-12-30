55994afffcd3de5577b38a63a2c8cb3ab47a4618
Giulio Carvalho <giulioccavalcante@gmail.com>
2021-12-30 00:01:56 -0300

    Add pipx packages installation

--- scripts/101-python.sh
+++ scripts/101-python.sh
@@ -15,37 +15,98 @@ install_python_version() {
         echo "#### The version "$1" is already installed"
         echo "################################################################"
         tput sgr0
-	else
+    else
         # "CC=clang" to avoid pip segmentation fault when installing on Arch Linux
-		CC=clang pyenv install -s $1
+        CC=clang pyenv install -s $1
 
-		echo
-		tput setaf 2
-		echo "################################################################"
-		echo "#### The version "$1" is now installed"
-		echo "################################################################"
-		tput sgr0
+        echo
+        tput setaf 2
+        echo "################################################################"
+        echo "#### The version "$1" is now installed"
+        echo "################################################################"
+        tput sgr0
     fi
 
 }
 
+
 install_python_versions() {
-	versions=(
-	2.7.18
-	3.6.15
-	3.7.12
-	3.8.12
-	3.9.9
-	)
+    versions=(
+    2.7.18
+    3.6.15
+    3.7.12
+    3.8.12
+    3.9.9
+    )
 
-	for version in "${versions[@]}"; do
+    for version in "${versions[@]}"; do
         install_python_version $version
-	done
+    done
 
-	echo
+    echo
     tput setaf 2
     echo "################################################################"
     echo "#### All Python versions are installed"
+    echo "################################################################"
+    tput sgr0
+}
+
+
+install_pipx() {
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    pip install --user pipx
+    pipx ensurepath
+
+    echo
+    tput setaf 2
+    echo "################################################################"
+    echo "#### Pipx is installed"
+    echo "################################################################"
+    tput sgr0
+}
+
+
+install_standalone_python_package() {
+    echo
+    tput setaf 7
+    echo "Installing Python package "$1
+    tput sgr0
+
+    if pipx list | grep $1 &> /dev/null; then
+        tput setaf 2
+        echo "################################################################"
+        echo "#### The package "$1" is already installed"
+        echo "################################################################"
+        tput sgr0
+    else
+        pipx install $1
+
+        echo
+        tput setaf 2
+        echo "################################################################"
+        echo "#### The package "$1" is now installed"
+        echo "################################################################"
+        tput sgr0
+    fi
+
+}
+
+
+install_standalone_python_packages() {
+    packages=(
+    mitmproxy
+    poetry
+    )
+
+    for package in "${packages[@]}"; do
+        install_standalone_python_package $package
+    done
+
+    echo
+    tput setaf 2
+    echo "################################################################"
+    echo "#### All standalone Python packages are installed"
     echo "################################################################"
     tput sgr0
 }
@@ -59,3 +120,22 @@ echo "################################################################"
 tput sgr0
 
 install_python_versions
+
+echo
+tput setaf 4;
+echo "################################################################"
+echo "Configuring pipx"
+echo "################################################################"
+tput sgr0
+
+install_pipx
+
+echo
+tput setaf 4;
+echo "################################################################"
+echo "Configuring standalone Python packages"
+echo "################################################################"
+tput sgr0
+
+install_standalone_python_packages
+
