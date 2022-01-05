fd59b6a402337583f781d6159f2c800661514a45
Giulio Carvalho <giulioccavalcante@gmail.com>
2022-01-05 01:30:31 -0300

    Modify polybar's weather module to get apikey from envvar

--- configs/polybar/home/.config/polybar/scripts/weather.py
+++ configs/polybar/home/.config/polybar/scripts/weather.py
@@ -1,37 +1,38 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
 
-# Procedure
-# Surf to https://openweathermap.org/city
+# Go to https://openweathermap.org/city
 # Fill in your CITY
-# e.g. Antwerp Belgium
+# e.g. Recife
 # Check url
-# https://openweathermap.org/city/2803138
+# https://openweathermap.org/city/3390760
 # you will the city code at the end
 # create an account on this website
 # create an api key (free)
-# LANG included thanks to krive001 on discord
 
+import os
 
 import requests
+from requests.exceptions import ConnectionError
 
 CITY = "3390760"  # Recife
-API_KEY = "***REMOVED***"
+API_KEY = os.environ["OPENWEATHERMAP_APIKEY"]  # Fill with your API key
 UNITS = "Metric"
 UNIT_KEY = "C"
-#UNIT_KEY = "F"
 LANG = "pt_br"
-#LANG = "nl"
-#LANG = "hu"
 
-REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
 try:
-    # HTTP CODE = OK
-    if REQ.status_code == 200:
-        CURRENT = REQ.json()["weather"][0]["description"].capitalize()
-        TEMP = int(float(REQ.json()["main"]["temp"]))
-        print("{}  {} °{}".format(CURRENT, TEMP, UNIT_KEY))
-    else:
-        print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
-except (ValueError, IOError):
-    print("Error: Unable print the data")
+    REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+except ConnectionError:
+    print("Error: Connection unavailable")
+else:
+    try:
+        # HTTP CODE = OK
+        if REQ.status_code == 200:
+            CURRENT = REQ.json()["weather"][0]["description"].capitalize()
+            TEMP = int(float(REQ.json()["main"]["temp"]))
+            print("{}  {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+        else:
+            print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
+    except (ValueError, IOError):
+        print("Error: Unable print the data")
