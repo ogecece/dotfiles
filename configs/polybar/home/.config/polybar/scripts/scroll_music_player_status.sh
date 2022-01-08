7da17ea2d24b88bd936fe902e93bcd41ad6d086a
Giulio Carvalho <giulioccavalcante@gmail.com>
2022-01-08 00:40:41 -0300

    Rename polybar's spotify2 module to music-player
    Also rename script names and all necessary references. The
    spotify1 module is also renamed to spotify since spotify2 doesn't
    exist anymore.

--- configs/polybar/home/.config/polybar/scripts/scroll_music_player_status.sh
+++ configs/polybar/home/.config/polybar/scripts/scroll_music_player_status.sh
@@ -5,7 +5,7 @@ zscroll \
   -l 60 \
   --delay 0.1 \
   --scroll-padding "    " \
-  --match-command "$HOME/.config/polybar/scripts/get_spotify_status.sh --status" \
+  --match-command "$HOME/.config/polybar/scripts/get_music_player_status.sh --status" \
   --match-text "No player is running" "" \
   --match-text "youtubemusic :: Stopped" "--before-text '   栗'" \
   --match-text "youtubemusic :: Paused" "--scroll 0 --before-text '   契    '" \
@@ -13,7 +13,7 @@ zscroll \
   --match-text "spotify :: Stopped" "--before-text '阮   栗'" \
   --match-text "spotify :: Paused" "--scroll 0 --before-text '阮   契    '" \
   --match-text "spotify :: Playing" "--scroll 1 --before-text '阮       '" \
-  --update-check true "$HOME/.config/polybar/scripts/get_spotify_status.sh" \
+  --update-check true "$HOME/.config/polybar/scripts/get_music_player_status.sh" \
   2>/dev/null &
 
 wait
